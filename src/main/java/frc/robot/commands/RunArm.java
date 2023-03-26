e49bd7a0ab5250c48e2b63e4d8915b0b2a06f898
The Rave Computer <team4601@gmail.com>
2023-03-26 13:22:44 -0400

    a

--- src/main/java/frc/robot/commands/RunArm.java
+++ src/main/java/frc/robot/commands/RunArm.java
@@ -19,7 +19,6 @@ public class RunArm extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
