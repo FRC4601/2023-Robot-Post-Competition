f612404fafdde8a88c34bce489ef458136941099
The Rave Computer <team4601@gmail.com>
2023-03-26 13:36:57 -0400

    gitkraken example

--- src/main/java/frc/robot/commands/RunArm.java
+++ src/main/java/frc/robot/commands/RunArm.java
@@ -19,6 +19,7 @@ public class RunArm extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //THIS IS HOW GITKRAKEN WORKS WOW ISN'T IT SO COOL?
   }
 
   // Called every time the scheduler runs while the command is scheduled.
